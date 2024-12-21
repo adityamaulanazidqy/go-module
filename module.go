dcb13d3c766b48dd9d4e9bf4596bf8ef5b907b41
adityamaulanazidqy <adityamaullana234@gmail.com>
2024-12-22 00:20:07 +0700

    Update : create 4 function

---
+++ module.go
@@ -0,0 +1,17 @@
+package gomodule
+
+func SayGoodbye(name string) string {
+	return "Goodbye, " + name + "!"
+}
+
+func SayHello(name string) string {
+	return "Hello, " + name + "!"
+}
+
+func DinnerTonight(name string) string {
+	return "Dinner tonight is" + name + "!"
+}
+
+func LunchToday(name string) string {
+	return "Lunch today is" + name + "!"
+}
